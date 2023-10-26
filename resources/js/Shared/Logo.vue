b694f2a23b91a089d8d53b16291dfe619b911ecd
Wilder Chero Torres <facturacion@innovofact.pe>
2023-10-26 17:54:04 -0500

    changes wilder

--- resources/js/Shared/Logo.vue
+++ resources/js/Shared/Logo.vue
@@ -1,6 +1,6 @@
 <template>
-    <img v-if="name === 'white'" class="logo w-[195px] h-auto" src="/images/logo_white.png" alt="Logo">
-    <img v-else class="logo w-[195px] h-auto" src="/images/logo.png" alt="Logo" />
+  <img v-if="name === 'white'" class="logo w-[195px] h-auto" src="/images/logo.png" alt="Logo" />
+  <img v-else class="logo w-[195px] h-auto" src="/images/logo.png" alt="Logo" />
 </template>
 <script>
 export default {

acc550c51bc3236fb8a03782fd9f6389d70be6cb
Wilder Chero Torres <facturacion@innovofact.pe>
2023-10-26 23:51:40 -0500

    spanish :)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        //Español por defecto :)
         app()->setLocale('es');
 
         Inertia::share([

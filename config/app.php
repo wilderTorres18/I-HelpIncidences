ee580a50c23cbf245e52ff0a8137c8af84a606f4
Wilder Chero Torres <facturacion@innovofact.pe>
2023-11-15 21:57:20 -0500

    Eliminando Laravel Passport

--- config/app.php
+++ config/app.php
@@ -154,7 +154,6 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
-        Laravel\Passport\PassportServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
